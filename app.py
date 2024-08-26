12265cb6fac413c17184c05561ffa0dcccf8e7a0
Kit Ho Chan <117521882+tyeckh@users.noreply.github.com>
2024-08-27 00:02:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 import google.generativeai as palm
 import os
 
-api = os.getenv("MAKERSUITE_API_TOKEN") 
+api = "AIzaSyAyK5yYWzpHhT7CRF7uxVkDh9NSjsVUrCU"
 palm.configure(api_key=api)
 model = {"model": "models/chat-bison-001"}
 
@@ -16,15 +16,19 @@ def index():
 def financial_QA():
     return(render_template("financial_QA.html"))
 
+@app.route("/financial_news",methods=["GET","POST"])
+def financial_news():
+    return(render_template("financial_news.html"))
+
 @app.route("/makersuite",methods=["GET","POST"])
 def makersuite():
     q = request.form.get("q")
     r = palm.chat(prompt=q, **model)
     return(render_template("makersuite.html",r=r.last))
 
-@app.route("/predcition",methods=["GET","POST"])
-def prediction():
-    return(render_template("predcition.html"))
+@app.route("/prediction",methods=["GET","POST"])
+def predcition():
+    return(render_template("prediction.html"))
 
 if __name__ == "__main__":
     app.run()
