d438f2bb46a05dcb59e500004746c8493417ba51
tyeckh <117521882+tyeckh@users.noreply.github.com>
2024-10-15 17:01:08 +0800

    update

--- app.py
+++ app.py
@@ -1,35 +1,48 @@
-from flask import Flask,render_template,request
-import google.generativeai as palm
+from flask import Flask, render_template, request
+import google.generativeai as genai
 import os
+import textblob
 
+# Configure API key
 api = os.getenv("MAKERSUITE_API_TOKEN") 
-palm.configure(api_key=api)
-model = { 'model': "models/text-bison-001"}
+genai.configure(api_key=api)
+
+# Load the model
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return(render_template("index.html"))
+    return render_template("index.html")
 
-@app.route("/financial_QA",methods=["GET","POST"])
+@app.route("/financial_QA", methods=["GET", "POST"])
 def financial_QA():
-    return(render_template("financial_QA.html"))
+    return render_template("financial_QA.html")
 
-@app.route("/financial_news",methods=["GET","POST"])
+@app.route("/financial_news", methods=["GET", "POST"])
 def financial_news():
-    return(render_template("financial_news.html"))
+    return render_template("financial_news.html")
 
-@app.route("/makersuite",methods=["GET","POST"])
+@app.route("/makersuite", methods=["GET", "POST"])
 def makersuite():
     q = request.form.get("q")
-    r = palm.generate_text(prompt=q, **model)
-    return(render_template("makersuite.html",r=r.result))
+    r = model.generate_content(q)  
+    return render_template("makersuite.html", r=r.text)
 
-@app.route("/prediction",methods=["GET","POST"])
-def prediction():
-    return(render_template("prediction.html"))
+@app.route("/sentiment_analysis", methods=["GET", "POST"])
+def sentiment_analysis():
+    return render_template("sentiment_analysis.html")
+
+@app.route("/transfer_money", methods=["GET", "POST"])
+def transfer_money():
+    return render_template("transfer_money.html")
+
+@app.route("/sentiment_analysis_result", methods=["GET", "POST"])
+def sentiment_analysis_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return render_template("sentiment_analysis_result.html", r=r)
 
 if __name__ == "__main__":
     app.run()
-
